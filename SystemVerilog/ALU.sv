a20b051f588ec92ae0399e96468227e576f7ce36
Rohan47 <152483882+RohanMuthyala47@users.noreply.github.com>
2025-06-20 19:28:45 +0530

    Update ALU.sv

--- SystemVerilog/ALU.sv
+++ SystemVerilog/ALU.sv
@@ -6,8 +6,8 @@ module alu (
 
     output logic [31:0] alu_result,
     output logic        branch_taken,
-    output logic [31:0] branch_trgt_pc,
-    output logic [31:0] jump_trgt_pc,
+    output logic [31:0] jal_trgt_pc,
+    output logic [31:0] jalr_trgt_pc,
     output logic        zero_flag
 );
 
@@ -17,31 +17,31 @@ module alu (
 
         case (alu_control)
             5'b00000: alu_result = op1 + op2;                                                                     // ADD, ADDI and LOAD
-            5'b00001: alu_result = op1 - op2;                                                                      // SUB
+            5'b00001: alu_result = op1 - op2;                                                                     // SUB
 
             5'b00010: alu_result = op1 ^ op2;                                                                     // XOR, XORI
             5'b00011: alu_result = op1 | op2;                                                                     // OR, ORI
-            5'b00100: alu_result = op1 & op2;                                                                    // AND, ANDI
+            5'b00100: alu_result = op1 & op2;                                                                     // AND, ANDI
 
-            5'b00101: alu_result = op1 << op2[4:0];                                                          // SLL, SLLI
-            5'b00110: alu_result = op1 >> op2[4:0];                                                          // SRL, SRLI
-            5'b00111: alu_result = $signed(op1) >>> op2[4:0];                                       // SRA, SRAI
+            5'b00101: alu_result = op1 << op2[4:0];                                                               // SLL, SLLI
+            5'b00110: alu_result = op1 >> op2[4:0];                                                               // SRL, SRLI
+            5'b00111: alu_result = $signed(op1) >>> op2[4:0];                                                     // SRA, SRAI
 
-            5'b01000: alu_result = ($signed(op1) < $signed(op2)) ? 32'd1 : 32'd0;     // SLT, SLTI
-            5'b01001: alu_result = (op1 < op2) ? 32'd1 : 32'd0;                                      // SLTU, SLTIU
+            5'b01000: alu_result = ($signed(op1) < $signed(op2)) ? 32'd1 : 32'd0;                                 // SLT, SLTI
+            5'b01001: alu_result = (op1 < op2) ? 32'd1 : 32'd0;                                                   // SLTU, SLTIU
 
-            5'b01010: branch_taken = (op1 == op2);                                                       // BEQ
-            5'b01011: branch_taken = (op1 != op2);                                                        // BNE
-            5'b01100: branch_taken = ($signed(op1) < $signed(op2));                       // BLT
-            5'b01101: branch_taken = ($signed(op1) >= $signed(op2));                     // BGE
-            5'b01110: branch_taken = (op1 < op2);                                                        // BLTU
-            5'b01111: branch_taken = (op1 >= op2);                                                     // BGEU
+            5'b01010: branch_taken = (op1 == op2);                                                                // BEQ
+            5'b01011: branch_taken = (op1 != op2);                                                                // BNE
+            5'b01100: branch_taken = ($signed(op1) < $signed(op2));                                               // BLT
+            5'b01101: branch_taken = ($signed(op1) >= $signed(op2));                                              // BGE
+            5'b01110: branch_taken = (op1 < op2);                                                                 // BLTU
+            5'b01111: branch_taken = (op1 >= op2);                                                                // BGEU
 
-            5'b10000: alu_result = op2 << 12;                                                                 // LUI
-            5'b10001: alu_result = pc + (op2 << 12);                                                      // AUIPC
+            5'b10000: alu_result = op2 << 12;                                                                     // LUI
+            5'b10001: alu_result = pc + (op2 << 12);                                                              // AUIPC
 
-            5'b10010: alu_result = pc + 4;                                                                       // JAL return address
-            5'b10011: alu_result = pc + 4;                                                                       // JALR return address
+            5'b10010: alu_result = pc + 4;                                                                        // JAL return address
+            5'b10011: alu_result = pc + 4;                                                                        // JALR return address
 
             default: begin
                 alu_result   = 32'b0;
@@ -52,7 +52,7 @@ module alu (
 
     assign zero_flag = (alu_result == 0);
     
-    assign branch_trgt_pc  = pc + op2;                   // JAL
-    assign jump_trgt_pc = (op1 + op2) & ~32'h1; // JALR
+    assign jal_trgt_pc  = pc + op2;             // JAL
+    assign jalr_trgt_pc = (op1 + op2) & ~32'h1; // JALR
 
 endmodule
