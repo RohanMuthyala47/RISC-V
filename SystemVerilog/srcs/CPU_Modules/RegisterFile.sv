3761360c4dca738d096b8febf7040780f96a1d4d
Rohan47 <rohanmuthyala1234@gmail.com>
2025-07-28 20:01:21 +0530

    Update RegisterFile.sv

--- SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
+++ SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
@@ -1,37 +1,40 @@
+`include "parameters.sv"
+
 module RegisterFile(
-    input logic clk,
-    input logic rst,
+    input logic                                    clk,
+    input logic                                    rst,
     
-    input logic [4:0] read_address1,
-    input logic [4:0] read_address2,
+    input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] read_address1,
+    input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] read_address2,
+        
+    input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] write_address,
+    input logic [DATA_WIDTH - 1:0]                 data,
+    input logic                                    write_enable,
     
-    output logic [31:0] read_data1,
-    output logic [31:0] read_data2,
-    
-    input logic [4:0] wr_address,
-    input logic [31:0] data,
-    input logic write_enable
+    output logic [DATA_WIDTH - 1:0]                read_data1,
+    output logic [DATA_WIDTH - 1:0]                read_data2
     );
     
-    logic [31:0] RegisterFile [0:31];
+    logic [DATA_WIDTH - 1:0] RegisterFile [0:REGISTER_FILE_SIZE - 1];
     integer i;
     
     always_ff @(posedge clk)
     begin
         if(rst)
         begin
-            for(i = 0;i < 32; i = i + 1)
+            for(i = 0;i < REGISTER_FILE_SIZE; i = i + 1)
                 RegisterFile[i] <= 'b0;
         end
         
-        else if(write_enable && wr_address != 0)
+        else if(write_enable && write_address != 0)
         begin
-            RegisterFile[wr_address] <= data;
+            RegisterFile[write_address] <= data;
         end
     end
     
     always_comb begin
-        read_data1 = (read_address1 == 0) ? 32'b0 : RegisterFile[read_address1];
-        read_data2 = (read_address2 == 0) ? 32'b0 : RegisterFile[read_address2];
-end
+        read_data1 = (read_address1 == 0) ? 'b0 : RegisterFile[read_address1];
+        read_data2 = (read_address2 == 0) ? 'b0 : RegisterFile[read_address2];
+    end
+
 endmodule
