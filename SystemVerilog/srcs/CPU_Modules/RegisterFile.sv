87422ee5658767d378d7ec9f895bce549709eea4
Rohan47 <rohanmuthyala1234@gmail.com>
2026-02-10 21:44:14 +0530

    Update RegisterFile.sv

--- SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
+++ SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
@@ -1,18 +1,18 @@
 `include "parameters.vh"
 
-module RegisterFile(
-    input logic                                    clk,
-    input logic                                    rst,
+module RegisterFile (
+    input logic clk,
+    input logic rst,
     
     input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] read_address1,
     input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] read_address2,
         
     input logic [$clog2(REGISTER_FILE_SIZE) - 1:0] write_address,
-    input logic [DATA_WIDTH - 1:0]                 data,
-    input logic                                    write_enable,
+    input logic [DATA_WIDTH - 1:0] data,
+    input logic write_enable,
     
-    output logic [DATA_WIDTH - 1:0]                read_data1,
-    output logic [DATA_WIDTH - 1:0]                read_data2
+    output logic [DATA_WIDTH - 1:0] read_data1,
+    output logic [DATA_WIDTH - 1:0] read_data2
     );
     
     logic [DATA_WIDTH - 1:0] RegisterFile [0:REGISTER_FILE_SIZE - 1];
@@ -23,7 +23,7 @@ module RegisterFile(
         if(rst)
         begin
             for(i = 0;i < REGISTER_FILE_SIZE; i = i + 1)
-                RegisterFile[i] <= 'b0;
+                RegisterFile[i] <= 32'b0;
         end
         
         else if(write_enable && write_address != 0)
@@ -33,8 +33,8 @@ module RegisterFile(
     end
     
     always_comb begin
-        read_data1 = (read_address1 == 0) ? 'b0 : RegisterFile[read_address1];
-        read_data2 = (read_address2 == 0) ? 'b0 : RegisterFile[read_address2];
+        read_data1 = (read_address1 == 0) ? 32'b0 : RegisterFile[read_address1];
+        read_data2 = (read_address2 == 0) ? 32'b0 : RegisterFile[read_address2];
     end
 
 endmodule
