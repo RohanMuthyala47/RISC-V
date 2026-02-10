e6a4600e247bb0115823054e00ff4eba23320268
Rohan47 <rohanmuthyala1234@gmail.com>
2026-02-10 21:43:01 +0530

    Update InstructionMemory.sv

--- SystemVerilog/srcs/CPU_Modules/InstructionMemory.sv
+++ SystemVerilog/srcs/CPU_Modules/InstructionMemory.sv
@@ -4,20 +4,17 @@ module InstructionMemory (
     input  logic                    clk,
     input  logic                    rst,
     input  logic [ADDR_WIDTH - 1:0] pc,
-    output logic [ADDR_WIDTH - 1:0] instruction
+    output logic [31:0]             instruction
 );
 
-    logic [ADDR_WIDTH - 1:0] InstructionMemory [0 : MEMORY_SIZE - 1];
+    logic [31:0] InstructionMemory [0 : MEMORY_SIZE - 1];
 
     initial begin
         $readmemh("program.hex", InstructionMemory);
     end
 
-    always_ff @(posedge clk) begin
-        if (rst)
-            instruction <= 32'd0;
-        else
-            instruction <= InstructionMemory[pc[ADDR_WIDTH - 1:2]];
+    always_comb begin
+        instruction <= InstructionMemory[pc[ADDR_WIDTH - 1:2]];
     end
 
 endmodule
