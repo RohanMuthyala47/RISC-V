37f652ab726fe2db2c810b0cbcbfe0d2a35be409
Rohan47 <rohanmuthyala1234@gmail.com>
2026-02-10 21:43:34 +0530

    Update ProgramCounter.sv

--- SystemVerilog/srcs/CPU_Modules/ProgramCounter.sv
+++ SystemVerilog/srcs/CPU_Modules/ProgramCounter.sv
@@ -5,7 +5,6 @@ module ProgramCounter (
     input  logic                    rst,
 
     input  logic                    branch_taken,
-    input  logic                    branch,
     input  logic [ADDR_WIDTH - 1:0] branch_target,
     
     input  logic                    is_jal,
@@ -16,30 +15,34 @@ module ProgramCounter (
     output logic [ADDR_WIDTH - 1:0] pc
 );
 
-    logic [31:0] next_pc;
+    logic [ADDR_WIDTH - 1:0] next_pc;
     
     // Set program counter to branch target if branch instruction,
     // jump target if jump instruction,
     // otherwise increment by 4
     always_comb begin
-        next_pc = pc + 'd4;
-        
+
         if (is_jal) begin
             next_pc = jal_target;
         end
+        
         else if (is_jalr) begin
             next_pc = jalr_target;
         end
         
-        else if (branch_taken && branch) begin
+        else if (branch_taken) begin
             next_pc = branch_target;
+        end
+        
+        else begin
+            next_pc = pc + 32'd4;
         end
     end
 
     // Update Program Counter
     always_ff @(posedge clk) begin
         if (rst)
-            pc <= 'd0;
+            pc <=32 'd0;
         else
             pc <= next_pc;
     end
