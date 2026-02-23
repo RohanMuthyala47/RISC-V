2d76da6e504c0dd7fa63c292b05de677371119ce
Rohan47 <rohanmuthyala1234@gmail.com>
2026-02-23 17:01:14 +0530

    Update InstructionMemory.sv

--- SystemVerilog/srcs/CPU_Modules/InstructionMemory.sv
+++ SystemVerilog/srcs/CPU_Modules/InstructionMemory.sv
@@ -1,9 +1,8 @@
 `include "parameters.vh"
 
 module InstructionMemory (
-    input  logic                    rst,
     input  logic [ADDR_WIDTH - 1:0] pc,
-    output logic [31:0]             instruction
+    output logic [31:0] instruction
 );
 
     logic [31:0] InstructionMemory [0 : MEMORY_SIZE - 1];
@@ -13,9 +12,7 @@ module InstructionMemory (
     end
 
     always_comb begin
-        /* verilator lint_off WIDTHTRUNC */
-	    instruction = InstructionMemory[pc[ADDR_WIDTH - 1:2]];
-	    /* verilator lint_on WIDTHTRUNC */
+            instruction <= InstructionMemory[pc[ADDR_WIDTH - 1:2]];
     end
 
 endmodule
