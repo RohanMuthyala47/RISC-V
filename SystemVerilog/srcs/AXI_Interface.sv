df8e500ab7c68f95ce1787ff3ef3e796e663022b
Rohan47 <rohanmuthyala1234@gmail.com>
2026-02-24 15:59:19 +0530

    Update AXI_Interface.sv

--- SystemVerilog/srcs/AXI_Interface.sv
+++ SystemVerilog/srcs/AXI_Interface.sv
@@ -9,15 +9,15 @@ interface AXI_interface #(
     logic ARESETn;
     
     //WRITE REQUEST CHANNEL
-    logic                    AWVALID; // Master address valid signal
-    logic                    AWREADY; // Slave ready signal
-    logic [ID_WIDTH - 1:0]   AWID;    // Transaction identifier
-    logic [ADDR_WIDTH - 1:0] AWADDR;  // Address write bus
-    logic [7:0]              AWLEN;   // Number of transactions - 1
-    logic [2:0]              AWSIZE;  // Size of each transaction in bytes
-    logic [1:0]              AWBURST; // Burst type (FIXED, INCR, WRAP)
-    // logic                 AWLOCK;     // Exclusive access indicator
-    // logic [3:0]           AWQOS;      // Quality of Service
+    logic                          AWVALID; // Master address valid signal
+    logic                          AWREADY; // Slave ready signal
+    logic [ID_WIDTH - 1:0]         AWID;    // Transaction identifier
+    logic [ADDR_WIDTH - 1:0]       AWADDR;  // Address write bus
+    logic [7:0]                    AWLEN;   // Number of transactions - 1
+    logic [2:0]                    AWSIZE;  // Size of each transaction in bytes
+    logic [1:0]                    AWBURST; // Burst type (FIXED, INCR, WRAP)
+    // logic                       AWLOCK;     // Exclusive access indicator
+    // logic [3:0]                 AWQOS;      // Quality of Service
     
     //WRITE DATA CHANNEL
     logic                          WVALID; // Master write valid signal
@@ -27,29 +27,29 @@ interface AXI_interface #(
     logic                          WLAST;  // Last transaction
     
     //WRITE RESPONSE CHANNEL
-    logic                  BVALID; // Master response valid signal
-    logic                  BREADY; // Slave ready signal
-    logic [ID_WIDTH - 1:0] BID;    // Transaction identifier
-    logic [1:0]            BRESP;  // Write response channel
+    logic                          BVALID; // Master response valid signal
+    logic                          BREADY; // Slave ready signal
+    logic [ID_WIDTH - 1:0]         BID;    // Transaction identifier
+    logic [1:0]                    BRESP;  // Write response channel
     
     //READ REQUEST CHANNEL
-    logic                    ARVALID; // Master address valid signal
-    logic                    ARREADY; // Slave ready signal
-    logic [ID_WIDTH - 1:0]   ARID;    // Transaction identifier
-    logic [ADDR_WIDTH - 1:0] ARADDR;  // Address read bus
-    logic [7:0]              ARLEN;   // Number of transactions - 1
-    logic [2:0]              ARSIZE;  // Size of each transaction in bytes
-    logic [1:0]              ARBURST; // Burst type (FIXED, INCR, WRAP)
-    // logic                 ARLOCK;     // Exclusive access indicator
-    // logic [3:0]           ARQOS;      // Quality of Service
+    logic                          ARVALID; // Master address valid signal
+    logic                          ARREADY; // Slave ready signal
+    logic [ID_WIDTH - 1:0]         ARID;    // Transaction identifier
+    logic [ADDR_WIDTH - 1:0]       ARADDR;  // Address read bus
+    logic [7:0]                    ARLEN;   // Number of transactions - 1
+    logic [2:0]                    ARSIZE;  // Size of each transaction in bytes
+    logic [1:0]                    ARBURST; // Burst type (FIXED, INCR, WRAP)
+    // logic                       ARLOCK;     // Exclusive access indicator
+    // logic [3:0]                 ARQOS;      // Quality of Service
     
     //READ DATA CHANNEL
-    logic                    RVALID; // Master read valid signal
-    logic                    RREADY; // Slave ready signal
-    logic [ID_WIDTH - 1:0]   RID;    // Transaction identifier
-    logic [DATA_WIDTH - 1:0] RDATA;  // Data read bus
-    logic [1:0]              RRESP;  // Read response channel
-    logic                    RLAST;  // Last transaction
+    logic                          RVALID; // Master read valid signal
+    logic                          RREADY; // Slave ready signal
+    logic [ID_WIDTH - 1:0]         RID;    // Transaction identifier
+    logic [DATA_WIDTH - 1:0]       RDATA;  // Data read bus
+    logic [1:0]                    RRESP;  // Read response channel
+    logic                          RLAST;  // Last transaction
     
     //-------------------------------------------------------------------------------------------//
     
