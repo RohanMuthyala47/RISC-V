8d4e6eb425c9e85581519e543d28e83168121aa1
Rohan47 <rohanmuthyala1234@gmail.com>
2026-03-03 17:37:41 +0530

    Update ImmediateSignExtender.sv

--- SystemVerilog/srcs/CPU_Modules/ImmediateSignExtender.sv
+++ SystemVerilog/srcs/CPU_Modules/ImmediateSignExtender.sv
@@ -10,7 +10,7 @@ module ImmediateSignExtender (
     always_comb begin
         case (opcode)
             I_TYPE, I_TYPE_LOAD, I_TYPE_JALR: // I-type
-            immediate = {{20{instruction[31]}}, instruction[31:20]};
+            immediate = {{21{instruction[31]}}, instruction[30:20]};
                 
             I_TYPE_SYS: // I-type ECALL and EBREAK
                 immediate = {DATA_WIDTH{1'b0}};
@@ -19,15 +19,13 @@ module ImmediateSignExtender (
                 immediate = {{21{instruction[31]}}, instruction[30:25], instruction[11:7]};
                 
             B_TYPE: // B-type
-                immediate = {{20{instruction[31]}}, instruction[7], 
-                          instruction[30:25], instruction[11:8], 1'b0};
+				immediate = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
                           
-	    U_TYPE_AUIPC, U_TYPE_LUI:
-            immediate = {instruction[31:12], 12'b0};
+	        U_TYPE_AUIPC, U_TYPE_LUI: // U-type
+                immediate = {instruction[31:12], 12'b0};
                 
             J_TYPE: // J-type JAL
-                immediate = {{12{instruction[31]}}, instruction[19:12], 
-                          instruction[20], instruction[30:21], 1'b0};
+				immediate = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
                 
             default: 
                 immediate = {DATA_WIDTH{1'b0}};
