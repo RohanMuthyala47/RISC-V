c8be23e3f1484efbe5872bd86a121bb3b4e6b869
Rohan47 <rohanmuthyala1234@gmail.com>
2026-03-06 14:20:43 +0530

    Update ImmediateSignExtender.sv

--- SystemVerilog/srcs/CPU_Modules/ImmediateSignExtender.sv
+++ SystemVerilog/srcs/CPU_Modules/ImmediateSignExtender.sv
@@ -2,10 +2,12 @@
 
 module ImmediateSignExtender (
     input  logic [ADDR_WIDTH - 1:0] instruction,
+	
     output logic [DATA_WIDTH - 1:0] immediate
 );
 
-    wire [6:0] opcode = instruction[6:0];
+	logic [6:0] opcode;
+	assign opcode = instruction[6:0];
 
     always_comb begin
         case (opcode)
@@ -21,10 +23,10 @@ module ImmediateSignExtender (
             B_TYPE: // B-type
 				immediate = {{20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0};
                           
-	        U_TYPE_AUIPC, U_TYPE_LUI: // U-type
+	    	U_TYPE_AUIPC, U_TYPE_LUI: // U-type
                 immediate = {instruction[31:12], 12'b0};
                 
-            J_TYPE: // J-type JAL
+            J_TYPE: // J-type
 				immediate = {{12{instruction[31]}}, instruction[19:12], instruction[20], instruction[30:21], 1'b0};
                 
             default: 
