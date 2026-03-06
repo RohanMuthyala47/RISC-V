9de38e5f9cb9e016c49239153f599520c4630d41
Rohan47 <rohanmuthyala1234@gmail.com>
2026-03-06 14:21:25 +0530

    Update DataMemory.sv

--- SystemVerilog/srcs/CPU_Modules/DataMemory.sv
+++ SystemVerilog/srcs/CPU_Modules/DataMemory.sv
@@ -4,12 +4,13 @@ module DataMemory (
     input  logic                    clk,
     input  logic                    rst,
     
+    input  logic [2:0]              funct3,
+    
     input  logic                    MemRead,
     input  logic                    MemWrite,
     
     input  logic [ADDR_WIDTH - 1:0] address, 
     input  logic [DATA_WIDTH - 1:0] write_data,
-    input  logic [2:0]              funct3,
     
     output logic [DATA_WIDTH - 1:0] read_data
 );
@@ -21,10 +22,9 @@ module DataMemory (
     logic [MEMORY_ADDR_WIDTH - 1:0] word_addr;
     assign word_addr = address[MEMORY_ADDR_WIDTH + 1:2];
     
-    always_ff @(posedge clk) begin
-        if(rst) begin
-            for(i = 0;i < REGISTER_FILE_SIZE; i = i + 1)
-                RegisterFile[i] <= {DATA_WIDTH{1'b0}};
+    initial begin
+        for (int i = 0; i < MEMORY_SIZE; i++) begin
+            DataMemory[i] = {DATA_WIDTH{1'b0}};
         end
     end
     
@@ -32,7 +32,6 @@ module DataMemory (
     always_ff @(posedge clk) begin
         if (MemWrite && word_addr < MEMORY_SIZE) begin
             case (funct3)
-                
                 3'b000: begin // SB (store byte)
                     case (address[1:0])
                         2'b00: DataMemory[word_addr][7:0]   <= write_data[7:0];
@@ -49,12 +48,10 @@ module DataMemory (
                     endcase
                 end
                 
-                3'b010: begin // SW (store word)
+                3'b010: // SW (store word)
                     DataMemory[word_addr] <= write_data;
-                end
-                
+                    
                 default: ;
-                
             endcase
         end
     end
@@ -64,7 +61,6 @@ module DataMemory (
     read_data = {DATA_WIDTH{1'b0}};
         if (MemRead && word_addr < MEMORY_SIZE) begin
             case (funct3)
-                
                 3'b000: begin // LB (load byte, sign-extended)
                     case (address[1:0])
                         2'b00: read_data = {{24{DataMemory[word_addr][7]}},  DataMemory[word_addr][7:0]};
@@ -81,10 +77,9 @@ module DataMemory (
                     endcase
                 end
                 
-                3'b010: begin // LW (load word)
+                3'b010: // LW (load word)
                     read_data = DataMemory[word_addr];
-                end
-                
+                    
                 3'b100: begin // LBU (load byte unsigned)
                     case (address[1:0])
                         2'b00: read_data = {24'b0, DataMemory[word_addr][7:0]};
@@ -102,9 +97,10 @@ module DataMemory (
                 end
                 
                 default: ;
-                
             endcase
-        end else begin
+        end 
+        
+        else begin
             read_data = {DATA_WIDTH{1'b0}};
         end
     end
