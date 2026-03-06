6d3e39c76e3dd08a1e48ef7abdd6bcb0ae7529b1
Rohan47 <rohanmuthyala1234@gmail.com>
2026-03-06 14:22:30 +0530

    Update RegisterFile.sv

--- SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
+++ SystemVerilog/srcs/CPU_Modules/RegisterFile.sv
@@ -1,18 +1,18 @@
 `include "parameters.svh"
 
 module RegisterFile(
-    input logic                                  clk,
-    input logic                                  rst,
+    input logic                                   clk,
+    input logic                                   rst,
     
-    input logic [REGISTER_FILE_ADDR_WIDTH - 1:0] read_address1,
-    input logic [REGISTER_FILE_ADDR_WIDTH - 1:0] read_address2,
+    input logic  [REGISTER_FILE_ADDR_WIDTH - 1:0] read_address1,
+    input logic  [REGISTER_FILE_ADDR_WIDTH - 1:0] read_address2,
         
-    input logic [REGISTER_FILE_ADDR_WIDTH - 1:0] write_address,
-    input logic [DATA_WIDTH - 1:0]               data,
-    input logic                                  write_enable,
+    input logic  [REGISTER_FILE_ADDR_WIDTH - 1:0] write_address,
+    input logic  [DATA_WIDTH - 1:0]               write_data,
+    input logic                                   write_enable,
     
-    output logic [DATA_WIDTH - 1:0]              read_data1,
-    output logic [DATA_WIDTH - 1:0]              read_data2
+    output logic [DATA_WIDTH - 1:0]               read_data1,
+    output logic [DATA_WIDTH - 1:0]               read_data2
     );
     
     logic [DATA_WIDTH - 1:0] RegisterFile [0:REGISTER_FILE_SIZE - 1];
@@ -20,12 +20,12 @@ module RegisterFile(
     
     always_ff @(posedge clk) begin
         if(rst) begin
-            for(i = 0;i < REGISTER_FILE_SIZE; i = i + 1)
+            for(i = 0; i < REGISTER_FILE_SIZE; i = i + 1)
                 RegisterFile[i] <= {DATA_WIDTH{1'b0}};
         end
         
         else if(write_enable && write_address != {REGISTER_FILE_ADDR_WIDTH{1'b0}}) begin
-            RegisterFile[write_address] <= data;
+            RegisterFile[write_address] <= write_data;
         end
     end
     
