62601e0ed93b3d6793e5ee6c0de540dc128afd99
Rohan47 <rohanmuthyala1234@gmail.com>
2026-03-07 20:51:31 +0530

    Create HazardForwarding_Unit.sv

---
+++ SystemVerilog/srcs/CPU_Modules/HazardForwarding_Unit.sv
@@ -0,0 +1,32 @@
+import cpu_pkg::*;
+
+module HazardForwarding_Unit (
+    input  logic [REG_ADDR_WIDTH - 1:0] rs1_E,
+    input  logic [REG_ADDR_WIDTH - 1:0] rs2_E,
+    
+    input  logic [REG_ADDR_WIDTH - 1:0] rd_M,
+    input  logic [REG_ADDR_WIDTH - 1:0] rd_WB,
+    
+    output logic [1:0]                  ForwardA,
+    output logic [1:0]                  ForwardB
+);
+
+    always_comb begin
+        if ((rd_M != 0) && (rd_M == rs1_E))
+            ForwardA = 2'b10;
+        else if ((rd_WB != 0) && (rd_WB == rs1_E))
+            ForwardA = 2'b01;
+        else
+            ForwardA = 2'b00;
+    end
+
+    always_comb begin
+        if ((rd_M != 0) && (rd_M == rs2_E))
+            ForwardB = 2'b10;
+        else if ((rd_WB != 0) && (rd_WB == rs2_E))
+            ForwardB = 2'b01;
+        else
+            ForwardB = 2'b00;
+    end
+
+endmodule
